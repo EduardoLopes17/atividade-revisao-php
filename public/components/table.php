c47220c15541cea7d3bc109ac9323d54f7f5e481
EduardoLopesx8 <eduardo.lopesx6@gmail.com>
2026-06-02 20:06:51 -0300

    table.php - Criação da tabela dos Usuarios, lincado com o banco de dados

--- public/components/table.php
+++ public/components/table.php
@@ -1,7 +1,11 @@
+
+<!--Titulo da pagina-->
 <h3>Usuários Cadastrados</h3>
 
+<!--Tabela com os usuários cadastrados-->
 <table border="1" cellpadding="3">
 
+    <!--Cabeçalho da tabela-->
     <tr>
         <th>ID</th>
         <th>Nome</th>
@@ -10,11 +14,14 @@
 
     <?php
     
+    // Consulta para selecionar todos os usuários cadastrados
     $sqlUsuarios = "SELECT * FROM users";
     $resultadoUsuarios = $conn->query($sqlUsuarios);
     
+    // Loop para ver os resultados da consulta
     while($linha = $resultadoUsuarios->fetch_assoc()){
 
+    // Exibe os dados de cada usuário em uma linha da tabela
         echo "
         <tr>
             <td>" . $linha["id"]."</td>
