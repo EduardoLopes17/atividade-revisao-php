fb3bf4aef211cc76d09db48d07d77214d0154db1
EduardoLopesx8 <eduardo.lopesx6@gmail.com>
2026-06-02 20:03:53 -0300

    home.php - Criação para o usuario consiguir se cadastrar, lincado com o banco de dados

--- public/home.php
+++ public/home.php
@@ -5,6 +5,7 @@
 
     // Se a sessão do usuário não estiver definida
     if(!isset($_SESSION["usuario"])){
+        
     // ele envia o usuario para outra pagina
         header("Location: ../index.php");
         exit();
@@ -13,18 +14,27 @@
     // Inclui a Coneção com o banco
     include("../infra/db/connect.php");
 
-    //
+    // Verifica se o método de requisição é POST
     if($_SERVER["REQUEST_METHOD"] == "POST"){
 
+    // Recebe os dados do formulário
         $novoUsuario = $_POST["usuario"];
         $novaSenha = $_POST["senha"];
 
+    // Insere os dados no banco
         $sql = "INSERT INTO users(username, password) VALUES ('$novoUsuario','$novaSenha')";
 
+    // Verifica se a query foi executada com sucesso
         if($conn->query($sql) === TRUE){
 
+    // Exibe um alerta de sucesso
             echo "<script> alert('Usuário Cadastrado com Sucesso!');</script>";
 
+    // Redireciona para a mesma página para atualizar a tabela
+            header("Location: home.php");
+            exit();
+
+    // Se a query não foi executada com sucesso, exibe um alerta de erro
         }else{
             echo "<script> alert('Erro: Usuário Não Cadastrado!');</script>";  
         }
@@ -33,34 +43,51 @@
 
 ?>
 <html lang="en">
-<head>
-    <title>Home</title>
-    <link rel="stylesheet" href="../styles/style.css">
-</head>
+<!-- O código HTML começa aqui -->
+    <head>
+
+    <!-- Titulo da pagina-->
+        <title>Home</title>
+
+    <!-- Link para o arquivo de estilos CSS -->
+        <link rel="stylesheet" href="../styles/style.css">
+
+    </head>
 <body>
 
     <?php
+    // Inclui o navbar
     include("components/navbar.php")
     ?>
 
+    <!-- Exibe uma mensagem de boas-vindas ao usuário logado -->
     <h1>Bem vindo, <?php echo $_SESSION["usuario"]; ?> </h1>
 
+    <!-- Link para a página de logout -->
     <a href="logout.php">Sair</a>
 
     <hr>
+    <!-- Um titulo maior -->
     <h3>Cadastrar Novos Usuários</h3>
+
+    <!-- Formulário para cadastrar novos usuários -->
     <form method="POST">
+
+    <!-- Campos para o nome de usuário e senha -->
         <label>Usuario</label>
         <input type="text" name="usuario"> <br>
         <label>Senha</label>
         <input type="password" name="senha"> <br>
         <br>
+
+    <!-- Botão para enviar o formulário -->
         <button type="submit">cadastrar</button>
     </form>
 
     <hr>
 
     <?php
+    // Inclui a tabela de usuários
     include("components/table.php")
     ?>
 
